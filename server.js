52554f127cc11c32d0eddf442444a105acdeb386
PraveenKrGit <30688435+PraveenKrGit@users.noreply.github.com>
2017-08-04 09:56:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/developers.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'developers.png'));
+app.get('/ui/codeArt.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'codeArt.png'));
 });
 
 
